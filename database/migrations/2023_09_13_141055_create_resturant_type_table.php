73d1667bcec0b2f96312c25949769bec8284a6f4
IanzanoLorenzo <ianzano.lorenzo@gmail.com>
2023-09-13 16:15:35 +0200

    creata migration pivot resturant_type

---
+++ database/migrations/2023_09_13_141055_create_resturant_type_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('resturant_type', function (Blueprint $table) {
+            $table->unsignedBigInteger('resturant_id');
+            $table->foreign('resturant_id')->references('id')->on('resturants');
+            $table->unsignedBigInteger('type_id');
+            $table->foreign('type_id')->references('id')->on('types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('resturant_type');
+    }
+};
