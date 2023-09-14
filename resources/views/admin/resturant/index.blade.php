abc9d0a5f704ed75f825aa495755a849f8b2ba7a
SimoneVilardo <simovila95@gmail.com>
2023-09-14 18:29:50 +0200

    [STYLE] index

--- resources/views/admin/resturant/index.blade.php
+++ resources/views/admin/resturant/index.blade.php
@@ -1,11 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1> Benvenuto {{ $user->name }}</h1>
-    <h2>{{ $resturant->name }}</h2>
-    @foreach ($resturant->dishes as $item)
-    <p>
-            {{$item->name}}
-    </p>
+    <div class="container">
+        <div class="row">
+            <h1> Benvenuto {{ $user->name }}</h1>
+            <h2>{{ $resturant->name }}</h2>
+        </div>
+        @foreach ($resturant->dishes as $item)
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <h4>
+                            {{$item->name}}
+                    </h4>
+                    <p>
+                        {{ $item->ingredients}}
+                    </p>
+                    <p>
+                        {{ $item->price}}€
+                    </p>
+                </div>
+            </div>
+        </div>
         @endforeach
+    </div>
 @endsection
