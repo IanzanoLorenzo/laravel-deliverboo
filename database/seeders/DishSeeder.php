6dad67ffa5879dd582040f7d995002f7dc057ba8
IanzanoLorenzo <ianzano.lorenzo@gmail.com>
2023-09-14 16:58:59 +0200

    creazione seeder dishes

--- database/seeders/DishSeeder.php
+++ database/seeders/DishSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Dish;
 
 class DishSeeder extends Seeder
 {
@@ -14,6 +15,16 @@ class DishSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $dishes = config('dishes');
+
+        foreach($dishes as $item){
+            $dish = new Dish();
+            $dish->name = $item['name'];
+            $dish->ingredients = $item['ingredients'];
+            $dish->price = $item['price'];
+            $dish->visibility = $item['visibility'];
+            $dish->resturant_id = $item['resturant_id'];
+            $dish->save();
+        }
     }
 }
--- resources/views/admin/resturant/index.blade.php
+++ resources/views/admin/resturant/index.blade.php
@@ -3,7 +3,9 @@
 @section('content')
     <h1> Benvenuto {{ $user->name }}</h1>
     <h2>{{ $resturant[0]->name }}</h2>
+    @foreach ($resturant[0]->dishes as $item)
     <p>
-        
+            {{$item->name}}
     </p>
+        @endforeach
 @endsection
