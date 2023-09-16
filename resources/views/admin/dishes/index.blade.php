c6a84cc77a2ec3d2246751c6a7fc4bc275d227dd
IanzanoLorenzo <ianzano.lorenzo@gmail.com>
2023-09-16 18:53:13 +0200

    cambiata table

--- resources/views/admin/dishes/index.blade.php
+++ resources/views/admin/dishes/index.blade.php
@@ -11,44 +11,48 @@
         <a href="{{route('admin.dishes.create')}}" class="btn btn-primary">Nuovo Piatto</a>
     </div>
     <div class="col-12">
-        <table class="table m-0">
-            <thead>
-                <tr>
-                    <th scope="col">
-                        Nome Piatto
-                    </th>
-                    <th scope="col">
-                        Prezzo
-                    </th>
-                    <th scope="col">
-                        Visibile
-                    </th>
-                    <th class="text-center" scope="col">
-                        Azioni
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($resturant->dishes as $dish)
-                <tr>
-                    <td>{{$dish->name}}</td>
-                    <td>{{$dish->price}}&euro;</td>
-                    <td>{{ $dish->visibility ? 'Sì' : 'No' }}</td>
-                    <td class="text-center">
-                        <a href="{{ route('admin.dishes.show', $dish ) }}" class="btn btn-primary"><i class="fa-solid fa-arrow-right"></i></a>
-                        <a href="{{ route('admin.dishes.edit', $dish) }}" class="btn btn-warning"><i class="fa-solid fa-pencil"></i></a>
-                        <form method="POST" class="d-inline-block dish-delete-button" action="{{ route('admin.dishes.destroy', $dish) }}" data-dish-name={{$dish->name}}>
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger">
-                                <i class="fa-solid fa-trash"></i>
-                            </button>
-                        </form>
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>    
+        <div class="card">
+            <div class="card-body pb-0">
+                <table class="table m-0">
+                    <thead>
+                        <tr>
+                            <th scope="col">
+                                Nome Piatto
+                            </th>
+                            <th scope="col">
+                                Prezzo
+                            </th>
+                            <th scope="col">
+                                Visibile
+                            </th>
+                            <th class="text-center" scope="col">
+                                Azioni
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($resturant->dishes as $dish)
+                        <tr>
+                            <td>{{$dish->name}}</td>
+                            <td>{{$dish->price}}&euro;</td>
+                            <td>{{ $dish->visibility ? 'Sì' : 'No' }}</td>
+                            <td class="text-center">
+                                <a href="{{ route('admin.dishes.show', $dish ) }}" class="btn btn-primary"><i class="fa-solid fa-arrow-right"></i></a>
+                                <a href="{{ route('admin.dishes.edit', $dish) }}" class="btn btn-warning"><i class="fa-solid fa-pencil"></i></a>
+                                <form method="POST" class="d-inline-block dish-delete-button" action="{{ route('admin.dishes.destroy', $dish) }}" data-dish-name={{$dish->name}}>
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger">
+                                        <i class="fa-solid fa-trash"></i>
+                                    </button>
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>    
+            </div>
+        </div>
     </div>
 </div>
 @include('admin.partials.model_dish_delete')
