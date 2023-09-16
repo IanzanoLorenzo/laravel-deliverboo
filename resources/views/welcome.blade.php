e25437440da70c9568f580bdf36a252235f52254
S-Lombardi <silo.lombardi@gmail.com>
2023-09-16 18:38:17 +0200

    modifiche grafica

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,38 +3,41 @@
 
 @section('content')
 
-<div class="bg-danger px-5">
     {{-- JUMBOTRON --}}
-    <div class="container py-5 ">
+    <div class="container-fluid">
+        
         {{-- Titolone --}}
-        <h1 class="text-light display-5 fw-bold">
-            Ben arrivato nel tuo gestionale di <em class="text-dark">DeliveBoo!</em>
+        <h1 class="text-light display-5 fw-bold text-center mt-4">
+            Ben arrivato nel tuo gestionale di <em class="text-dark shadow-title">DeliveBoo!</em>
         </h1>
 
-        {{-- STRISCIA NERA --}}
-        <div class="">
+        <div class="container py-5 ">
             <div class="row">
-            <div class="col-12 col-md-6 d-flex align-items-center">
-                {{-- Paragrafo --}}
-                <p class="text-light fs-5">
-                    Se hai mai desiderato semplificare la gestione del tuo ristorante o servizio di 
-                    consegna di cibo a domicilio, sei nel posto giusto. Il nostro potente sistema di gestione 
-                    è stato progettato su misura per le tue esigenze, offrendoti la soluzione completa per rendere 
-                    la tua attività più efficiente, redditizia e soddisfacente.
-                </p>
+                <div class="col-12 col-md-6 d-flex align-items-center">
+                    {{-- Paragrafo --}}
+                    <p class="text-dark fs-5">
+                        Se hai mai desiderato semplificare la gestione del tuo ristorante o servizio di 
+                        consegna di <strong class="text-light">cibo a domicilio</strong>, sei nel posto giusto.
+                        <br> 
+                        <br>
+                        Il nostro potente sistema di gestione 
+                        è stato progettato su misura per le tue esigenze, offrendoti la <strong class="text-light">soluzione completa</strong> per rendere 
+                        la tua attività più efficiente e redditizia.
+                    </p>
+                </div> 
+                <div class="col-12 col-md-6">
+                    {{-- Immagine gestionale --}}
+                    <img src="{{ asset('storage/img/gestionale_home_def.jpg') }}" alt="">
+                </div>
             </div>
-            <div class="col-12 col-md-6">
-                {{-- Immagine gestionale --}}
-                <img src="{{ asset('storage/img/gestionale_home_def.jpg') }}" alt="">
-            </div>
+            {{-- Pulsante Registrati --}}
+            @if (Route::has('register'))
+                <a href="{{ route('register')}}" class="button-delive btn btn-lg" type="button">
+                    Registrati
+                </a>
+            @endif
+            
         </div>
-        {{-- Pulsante Registrati --}}
-        @if (Route::has('register'))
-        <a href="{{ route('register')}}" class="btn btn-primary btn-lg" type="button">Registrati</a>
-        @endif
-        </div>
+        {{-- FINE JUMBOTRON --}}
     </div>
-    {{-- FINE JUMBOTRON --}}
-</div>
-
 @endsection
