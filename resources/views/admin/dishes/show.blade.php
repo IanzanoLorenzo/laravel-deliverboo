3361d4f9a57d938dc2fe8819e9195c71e85d8ec9
S-Lombardi <silo.lombardi@gmail.com>
2023-09-18 18:40:08 +0200

    Grafica show

--- resources/views/admin/dishes/show.blade.php
+++ resources/views/admin/dishes/show.blade.php
@@ -1,26 +1,47 @@
 @extends('layouts.admin')
 
 @section('content')
-<div class="container card text-dark bg-info my-5">
+<div class="container">
+    {{-- PULSANTE --}}
     <div class="my-4 d-flex justify-content-end">
-        <a href="{{route('admin.dishes.index')}}" class="btn btn-sm btn-primary">Torna ai piatti</a>
+        <a href="{{route('admin.dishes.index')}}" class="btn btn-sm btn-primary">
+            Torna ai piatti
+        </a>
     </div>
-    <div class="card-header">
-        <h1>{{$dish->name}}</h1>
-    </div>
-    @if (session('message'))
-        <div class="alert alert-success">
-            {{session('message')}}
+    <h2 class="text-white text-center">Visualizza i dettagli del tuo piatto</h2>
+    <div class="card my-5 p-4">
+
+        {{-- NOME PIATTO --}}
+        <div class="card-header d-flex justify-content-between align-items-center">
+            <h1 class="text-primary">{{$dish->name}}</h1>
+            
+            <h3>&euro;{{$dish->price}}</h3>
         </div>
-    @endif
-    <div class="row">
+        @if (session('message'))
+            <div class="alert alert-success">
+                {{session('message')}}
+            </div>
+        @endif
+        <div class="row">
+        
+            <div class="col-12 card-body">
+                
+                <span class="text-dark">
+                    Ingredienti:
+                </span>  
+                <strong> 
+                    {{$dish->ingredients}}
+                </strong>
+                
+                
+                <div>
+                    {{$dish->visibility ? 'Il piatto è visibile sul sito' : 'Il piatto non è visibile  sul sito'}}
+                </div>
+            </div>
+        </div>
+
+    </div>
     
-        <div class="col-12 card-body">
-            <p><strong>ingredienti: {{$dish->ingredients}}</strong></p>
-            <h3>prezzo:{{$dish->price}}€</h3>
-            <p>{{$dish->visibility ? 'Il piatto è visibile sul sito' : 'Il piatto non è visibile  sul sito'}}</p>
-        </div>
-    </div>
 </div>
 @endsection
     
