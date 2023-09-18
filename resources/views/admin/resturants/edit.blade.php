b7400c485b8b5df53a23dcb626587a60e2bf651f
IanzanoLorenzo <ianzano.lorenzo@gmail.com>
2023-09-18 18:11:02 +0200

    cambiata classe img edit resturant

--- resources/views/admin/resturants/edit.blade.php
+++ resources/views/admin/resturants/edit.blade.php
@@ -60,7 +60,7 @@
                         @error('cover_image')
                             <div class="text-danger">{{ $message }}</div>
                         @enderror 
-                        <img class="img-thumbnail rounded-circle img_resturant my-3" src="{{asset('storage/'.$resturant->cover_image)}}" alt="">
+                        <img class="img-thumbnail rounded-circle img_resturant mt-3" src="{{asset('storage/'.$resturant->cover_image)}}" alt="">
                     </div>
                     {{-- PULSANTE --}}
                     <button type="submit" class="btn btn-primary m-3">
