ddd56046a753f4f3457905ad1540baa02029a89f
IanzanoLorenzo <ianzano.lorenzo@gmail.com>
2023-09-21 18:35:05 +0200

    aggiunti tipi ad api resturant

--- app/Http/Controllers/Api/ResturantController.php
+++ app/Http/Controllers/Api/ResturantController.php
@@ -5,15 +5,21 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 Use App\Models\Resturant;
+Use App\Models\Type;
+
 
 class ResturantController extends Controller
 {
     public function index(){
         $resturants = Resturant::with('types')->get();
+        $types = Type::all();
         return response()->json(
             [
                 'success' => true,
-                'response' => $resturants
+                'response' => [
+                    'resturants' => $resturants,
+                    'types' => $types
+                ]
             ]
             );
     }
