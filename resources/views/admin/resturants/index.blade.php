c026dd47307a3edb7d1477211a735d0d96cdfd9d
S-Lombardi <silo.lombardi@gmail.com>
2023-09-22 17:23:39 +0200

    Aggiunta legenda lato mobile, modifica pulsante e scritta di benvenuto

--- resources/views/admin/resturants/index.blade.php
+++ resources/views/admin/resturants/index.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         {{-- UTENTE RISTORATORE --}}
         <h2 class="text-center text-white">
-            Benvenuto 
+            Salve 
             <strong class="text-dark">{{ $user->name }}</strong>
             !
         </h2>
@@ -14,18 +14,18 @@
             <em class="text-dark display-6">{{ $resturant->name }}</em>
             .
         </h4>
-
+        <hr class="text-white hr_style mb-4">
         <div class="row justify-content-center">
             <div class="col-12 col-md-4 d-flex flex-column justify-content-center">
                 {{-- TIPI RISTORANTE --}}
-                <ul class="list-unstyled text-center text-md-end">
+                <ul class="list-unstyled text-center text-md-end text-uppercase text-white fs-5">
                     @forelse ($resturant->types as $type)
                     <li>
+                        <i class="fs-3 me-1 fa {{$type->icon}}"></i> 
                         <span>{{$type->name}}</span> 
-                        <i class="fs-3 ms-1 fa {{$type->icon}}"></i> 
                     </li>
                     @empty
-                    <li class="bg-white p-1 text-danger">
+                    <li class="bg-white p-1 text-danger text-uppercase">
                         Nessun tipo selezionato
                     </li>
                     @endforelse
@@ -39,15 +39,15 @@
             <div class="col-12 col-md-4 d-flex justify-content-center align-items-center">
 
                 <div class="d-flex mt-4 mt-md-0">
-                    {{-- PULSANTE EDIT PIATTI  --}}
-                    <a href="{{ route('admin.dishes.index')}}" class="button_delive_two btn btn-sm me-3" type="button">
-                        Modifica Piatti
-                        <i class="fa-solid fa-arrow-turn-up ms-1" style="color: #ffffff;"></i>
-                    </a>
                     {{-- PULSANTE EDIT RISTORANTE --}}
-                    <a href="{{ route('admin.resturants.edit')}}" class="button_delive_two btn btn-sm d-flex align-items-center" type="button">
+                    <a href="{{ route('admin.resturants.edit')}}" class="me-3 button_delive_three btn btn-sm d-flex align-items-center" type="button">
                         Modifica Ristorante
-                        <i class="fa-solid fa-arrow-turn-up ms-1" style="color: #ffffff;"></i>
+                        <i class="fa-solid fa-pencil ms-1" style="color: #ffffff;"></i>
+                    </a>
+                    {{-- PULSANTE EDIT PIATTI  --}}
+                    <a href="{{ route('admin.dishes.index')}}" class="button_delive_two btn btn-sm" type="button">
+                        Modifica Piatti
+                        <i class="fa-solid fa-pencil ms-1" style="color: #ffffff;"></i>
                     </a>
                 </div>
                 
@@ -61,7 +61,7 @@
 
         
         <div class="row mb-5 mt-5 position-relative">
-            {{-- LEGENDA --}}
+            {{-- LEGENDA TABLET--}}
             <div class="col-2 position_tr d-none d-md-block">
                 <div class="card text-center">
                     <div class="bg-primary text-white p-1">Legenda</div>
@@ -69,18 +69,25 @@
                     <div class="back-nv p-1">Non Visibile</div>
                 </div>
             </div>
-            {{-- LEGENDA --}}
+            {{-- FINE LEGENDA --}}
             <div class="col-12 col-md-8 offset-md-2 px-4">
         <div class="row mb-5 mt-5">
             <div class="col-12 col-md-8 offset-md-2">
                 @if(count($resturant->dishes) !== 0)
                     
                     <ul class="list-group list-group-flush">
-                        {{-- MENU --}}
+                        {{-- INIZIO MENU --}}
                         <li class="list-group-item">
                             <h2 class="text-center">
+                                
                                 Men&uacute;
+                                
                             </h2>
+                            {{-- LEGENDA MOBILE--}}
+                            <div class="d-block d-md-none border">
+                                <div class="bg-white p-1">Visibile</div>
+                                <div class="back-nv p-1">Non Visibile</div>
+                            </div>
                         </li>
                         @foreach ($resturant->dishes as $item)
                         <li class="list-group-item @if(!$item->visibility) list-group-item-secondary @endif">
