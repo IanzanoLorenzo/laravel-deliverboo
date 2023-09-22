86c993667d26a4836a9f046bcb48d4d2896a1dd2
IanzanoLorenzo <ianzano.lorenzo@gmail.com>
2023-09-22 17:00:05 +0200

    aggiunti ristoranti

--- config/resturants.php
+++ config/resturants.php
@@ -1,49 +1,86 @@
 <?php
 
-return [
-    //Ristorante italiano
+$restaurants = [
     [
         'user_id' => 1,
-
-        'name' => 'Piero e Anna',
-        
-        'slug' => 'piero-e-anna',
-        
+        'name' => 'Piero e Anna Ristorante',
+        'slug' => 'piero-e-anna-ristorante',
         'address' => 'Via Roma, 18',
-
         'cover_image' => 'cover_images/ristorante_italiano.jpg',
-
         'types' => [1, 3]
-
     ],
-    //Ristorante giapponese
     [
         'user_id' => 2,
-
-        'name' => 'Ristorante Giapponese',
-        
-        'slug' => 'ristorante-giapponese',
-        
+        'name' => 'Ristorante Giapponese di Bruce',
+        'slug' => 'ristorante-giapponese-di-bruce',
         'address' => 'Via Amerigo Vespucci, 17',
-
         'cover_image' => 'cover_images/ristorante_giapponese.jpg',
-
         'types' => [1, 2, 3]
-
     ],
-    //Ristorante messicano
     [
         'user_id' => 3,
-
-        'name' => 'Casa Mezcal',
-        
-        'slug' => 'casa-mezcal',
-        
+        'name' => 'Casa Mezcal Ristorante',
+        'slug' => 'casa-mezcal-ristorante',
         'address' => 'Via Tommaso, 20',
-
         'cover_image' => 'cover_images/ristorante_messicano.jpg',
-
         'types' => [3]
     ],
-]
-?>
+    [
+        'user_id' => 4,
+        'name' => 'Ristorante Greco di Maria',
+        'slug' => 'ristorante-greco-di-maria',
+        'address' => 'Via Grecia, 12',
+        'cover_image' => 'cover_images/ristorante_greco.jpg',
+        'types' => [1]
+    ],
+    [
+        'user_id' => 5,
+        'name' => 'Sapori d\'India Ristorante',
+        'slug' => 'sapori-d-india-ristorante',
+        'address' => 'Via Delhi, 5',
+        'cover_image' => 'cover_images/ristorante_indiano.jpg',
+        'types' => [1, 4]
+    ],
+    [
+        'user_id' => 6,
+        'name' => 'Le Bistro Ristorante',
+        'slug' => 'le-bistro-ristorante',
+        'address' => 'Rue de Paris, 9',
+        'cover_image' => 'cover_images/ristorante_francese.jpg',
+        'types' => [5]
+    ],
+    [
+        'user_id' => 7,
+        'name' => 'Trattoria Maria',
+        'slug' => 'trattoria-maria',
+        'address' => 'Via Napoli, 27',
+        'cover_image' => 'cover_images/ristorante_italiano.jpg',
+        'types' => [1, 3]
+    ],
+    [
+        'user_id' => 8,
+        'name' => 'Sushi Express',
+        'slug' => 'sushi-express',
+        'address' => 'Tokyo Street, 8',
+        'cover_image' => 'cover_images/ristorante_giapponese.jpg',
+        'types' => [2, 3]
+    ],
+    [
+        'user_id' => 9,
+        'name' => 'Taqueria Mexico',
+        'slug' => 'taqueria-mexico',
+        'address' => 'Avenida de los Tacos, 15',
+        'cover_image' => 'cover_images/ristorante_messicano.jpg',
+        'types' => [3]
+    ],
+    [
+        'user_id' => 10,
+        'name' => 'Greek Delights',
+        'slug' => 'greek-delights',
+        'address' => 'Athens Square, 14',
+        'cover_image' => 'cover_images/ristorante_greco.jpg',
+        'types' => [1, 3]
+    ],
+];
+
+return $restaurants;
