ac24b8b859098d8f9c9391de51ff6dd4497cae63
IanzanoLorenzo <ianzano.lorenzo@gmail.com>
2023-09-23 11:44:05 +0200

    modificato api piatti

--- app/Http/Controllers/Api/DishController.php
+++ app/Http/Controllers/Api/DishController.php
@@ -11,12 +11,14 @@ class DishController extends Controller
 {
     public function index(String $slug){
         $resturant = Resturant::where('slug', $slug)->get()->first();
-        $dishes = Dish::where('resturant_id', $resturant->id, '')->where('visibility', true)->get();
+        $dishes = Dish::where('resturant_id', $resturant->id)->where('visibility', true)->get();
         return response()->json(
             [
                 'success' => true,
-                'response' => $dishes,
-                'resturant' => $resturant->id
+                'response' => [
+                    'dishes' => $dishes,
+                    'resturant' => $resturant->id
+                ]
             ]
             );
     }
