39fbdf52b6c6f99f15508b5714d22569c43c1591
IanzanoLorenzo <ianzano.lorenzo@gmail.com>
2023-09-25 10:20:46 +0200

    gestione salvataggio ordine

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -5,22 +5,29 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Order;
+use App\Models\orderDishRelation;
 use App\Http\Requests\StoreOrderRequest;
 use App\Http\Requests\UpdateOrderRequest;
 use Braintree\Gateway;
 
 class OrderController extends Controller
 {
-    public function getToken(){
-        $gateway = new Gateway([
+
+    protected $gateway;
+
+    public function __construct()
+    {
+        $this->gateway = new Gateway([
             'environment' => config('services.braintree.environment'),
             'merchantId' => config('services.braintree.merchantId'),
             'publicKey' => config('services.braintree.publicKey'),
             'privateKey' => config('services.braintree.privateKey'),
         ]);
-        
+    }
+
+    public function getToken(){
         // pass $clientToken to your front-end
-        $clientToken = $gateway->clientToken()->generate();
+        $clientToken = $this->gateway->clientToken()->generate();
 
         return response()->JSON([
             'success' => true,
@@ -29,29 +36,51 @@ class OrderController extends Controller
             ]
         ]);
     }
-
+    
     public function processPayment(Request $request){
-        $responce = $request->all();
+        $formData = $request->all();
+        dd($formData);
+        $orderData = $formData['order'];
+        $dishesData = $formData['cart'];
 
-        $gateway = new Gateway([
-            'environment' => config('services.braintree.environment'),
-            'merchantId' => config('services.braintree.merchantId'),
-            'publicKey' => config('services.braintree.publicKey'),
-            'privateKey' => config('services.braintree.privateKey'),
-        ]);
-
-        $result = $gateway->transaction()->sale([
-            'amount' => '10.00',
-            'paymentMethodNonce' => $responce['nonce'],
+        $result = $this->gateway->transaction()->sale([
+            'amount' => $orderData['total_price'],
+            'paymentMethodNonce' => $formData['nonce'],
             'options' => [
               'submitForSettlement' => True
             ]
         ]);
 
-        return response()->json([
-            'success' => true,
-            'response' => 'Pagamento effettuato con successo',
-            'transactionId' => $result->transaction->id,
-        ]);
+        if($result->success){
+            $new_order = new Order;
+            $new_order->resturant_id = $orderData['resturant_id'];
+            $new_order->address = $orderData['address'];
+            $new_order->customer_email = $orderData['costumer_email'];
+            $new_order->customer_name = $orderData['costumer_name'];
+            $new_order->customer_surname = $orderData['costumer_surname'];
+            $new_order->delivery_time = $orderData['delivery_time'];
+            $new_order->total_price = $orderData['total_price'];
+            $new_order->save();
+
+            foreach($dishesData as $dish){
+                $order_dish = new orderDishRelation();
+                $order_dish->order_id = $new_order->id;
+                $order_dish->dish_id = $dish['id'];
+                $order_dish->n_dish = $dish['quantity'];
+                $order_dish->save();
+            };
+
+            return response()->json([
+                'success' => true,
+                'response' => 'Pagamento effettuato con successo',
+                'transactionId' => $result->transaction->id,
+            ]);
+        }else{
+            return response()->json([
+                'success' => false,
+                'response' => 'Pagamento annullato',            
+            ]);
+        }
+
     }
 }
