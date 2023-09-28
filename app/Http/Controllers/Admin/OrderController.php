9df4571467b38a1bb2448ab8fa1d6c7cc3d23b18
SimoneVilardo <simovila95@gmail.com>
2023-09-28 13:02:20 +0200

    inserimento pagina per riepilo ordini

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -13,74 +13,21 @@ class OrderController extends Controller
     *
     * @return \Illuminate\Http\Response
     */
-   public function index()
-   {
-       //
-   }
+    public function index()
+    {
+        $orders = Order::all();
 
-   /**
-    * Show the form for creating a new resource.
-    *
-    * @return \Illuminate\Http\Response
-    */
-   public function create()
-   {
-       //
-   }
-
-   /**
-    * Store a newly created resource in storage.
-    *
-    * @param  \Illuminate\Http\Request  $request
-    * @return \Illuminate\Http\Response
-    */
-   public function store(Request $request)
-   {
-       //
-   }
-
-   /**
-    * Display the specified resource.
-    *
-    * @param  \App\Models\Order  $order
-    * @return \Illuminate\Http\Response
-    */
-   public function show(Order $order)
-   {
-       //
-   }
-
-   /**
-    * Show the form for editing the specified resource.
-    *
-    * @param  \App\Models\Order  $order
-    * @return \Illuminate\Http\Response
-    */
-   public function edit(Order $order)
-   {
-       //
-   }
-
-   /**
-    * Update the specified resource in storage.
-    *
-    * @param  \Illuminate\Http\Request  $request
-    * @param  \App\Models\Order  $order
-    * @return \Illuminate\Http\Response
-    */
-   public function update(Request $request, Order $order)
-   {
-       //
-   }
-
-   /**
-    * Remove the specified resource from storage.
-    *
-    * @param  \App\Models\Order  $order
-    * @return \Illuminate\Http\Response
-    */
-   public function destroy(Order $order)
-   {
-       //
-   }
+        return view('admin.orders.index', compact('orders'));
+    }
+    
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Order  $order
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Order $order)
+    {
+        
+    }
 }
