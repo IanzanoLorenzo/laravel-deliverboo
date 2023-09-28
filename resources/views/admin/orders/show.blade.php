e5087df4470b70599d0c9d46a646b4473333689f
SimoneVilardo <simovila95@gmail.com>
2023-09-28 16:01:17 +0200

    inizio modifiche show

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -2,4 +2,36 @@
 
 @section('content')
 
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="card my-5 p-4">
+            
+                    {{-- NOME PIATTO --}}
+                    <div class="card-header d-flex justify-content-between align-items-center">
+                        <h1 class="text-primary">{{$order->customer_name}} {{ $order->customer_surname }}</h1>
+                        
+                        {{-- <h3>&euro;{{ number_format($dish->price, 2, ',','.') }}</h3> --}}
+                    </div>
+                    <div class="row">
+                    
+                        {{-- <div class="col-12 card-body">
+                            
+                            <span class="text-dark">
+                                Ingredienti:
+                            </span>  
+                            <strong> 
+                                {{$dish->ingredients}}
+                            </strong>
+                            
+                            
+                            <div>
+                                {{$dish->visibility ? 'Il piatto è visibile sul sito' : 'Il piatto non è visibile  sul sito'}}
+                            </div>
+                        </div> --}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
