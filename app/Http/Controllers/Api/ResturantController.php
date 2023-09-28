26116ca0245169b7d70e9857a1b053031765c476
IanzanoLorenzo <ianzano.lorenzo@gmail.com>
2023-09-28 10:06:53 +0200

    paginazione

--- app/Http/Controllers/Api/ResturantController.php
+++ app/Http/Controllers/Api/ResturantController.php
@@ -11,7 +11,7 @@ Use App\Models\Type;
 class ResturantController extends Controller
 {
     public function index(){
-        $resturants = Resturant::with('types')->get();
+        $resturants = Resturant::with('types')->paginate(4);
         $types = Type::all();
         return response()->json(
             [
